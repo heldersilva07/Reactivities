13f3c19eeae4175c3f10be21dfcd1710d72932e5
HelderSilva <silvahelder.03@gmail.com>
2020-12-22 10:24:39 +0000

    End of section 12

---
+++ Application/Users/CurrentUser.cs
@@ -0,0 +1,43 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Persistence;
+
+namespace Application.Users
+{
+    public class CurrentUser
+    {
+        public class Query : IRequest<User> { }
+
+        public class Handler : IRequestHandler<Query, User>
+        {
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJwtGenerator _jwtGenerator;
+            private readonly IUserAccessor _userAcessor;
+            public Handler(UserManager<AppUser> userManager, IJwtGenerator jwtGenerator, IUserAccessor userAcessor)
+            {
+                _userAcessor = userAcessor;
+                _jwtGenerator = jwtGenerator;
+                _userManager = userManager;
+
+            }
+
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
+            {
+                // handler logic goes here
+                var user = await _userManager.FindByNameAsync(_userAcessor.GetCurrentUsername());
+
+                return new User
+                {
+                    DisplayName = user.DisplayName,
+                    Username = user.UserName,
+                    Token = _jwtGenerator.CreateToken(user),
+                    Image = null
+                };
+            }
+        }
+    }
+}
